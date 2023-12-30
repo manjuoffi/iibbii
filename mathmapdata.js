1beceb2d83722fde27ea2b16a5b7d69d6d1756e1
manjuoffi <manjuoffi@gmail.com>
2023-12-30 08:09:14 +0530

    New template1

---
+++ mathmapdata.js
@@ -0,0 +1,19 @@
+const mathData = {
+  "stories": [
+    {
+      "title": "Math 1",
+      "author": "Math Author 1",
+      "content": "Exploring equations...",
+      "url": "http://www.mathsite.com"
+    },
+    {
+      "title": "Math 2",
+      "author": "Math Author 2",
+      "content": "Geometry journey...",
+      "url": "http://www.mathworld.com"
+    }
+    // Add more stories for the "Mathematics" section as needed
+  ]
+};
+
+export default mathData;
---
+++ storymappdata.js
@@ -0,0 +1,25 @@
+const storyData = {
+  "stories": [
+    {
+      "title": "Story 1",
+      "author": "Author 1",
+      "content": "Once upon a time...",
+      "url": "http://www.google.com"
+    },
+    {
+      "title": "Story 2",
+      "author": "Author 2",
+      "content": "In a distant land...",
+      "url": "http://www.example.com"
+    },
+    {
+      "title": "Story 3",
+      "author": "Author 3",
+      "content": "Another adventure...",
+      "url": "http://www.example2.com"
+    }
+    // Add more stories for the "Story" section as needed
+  ]
+};
+
+export default storyData;
